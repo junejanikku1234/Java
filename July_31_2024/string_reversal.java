6eb678a02cc00afb40f7489be93144a8648170e7
junejanikku1234 <23csu215@ncuinida.edu>
2024-07-31 11:49:54 +0530

    deleting stuff

--- July_31_2024/string_reversal.java
+++ July_31_2024/string_reversal.java
@@ -1,7 +1,17 @@
 package July_31_2024;
+import java.util.Scanner;
 
 public class string_reversal {
     public static void main(String[] args) {
-
+        Scanner con = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = con.nextLine();
+        char[] chararr = str.toCharArray();
+        char[] reversed = new char[chararr.length];
+        for(int i=0; i< chararr.length; i++){
+            reversed[i] = chararr[chararr.length - i - 1];
+        }
+        String output = String.valueOf(reversed);
+        System.out.println("The reversed string is: " + output);
     }
 }
