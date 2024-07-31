cfed42db3db217f85b194cf6de86bc1c664904dc
junejanikku1234 <23csu215@ncuinida.edu>
2024-07-31 10:52:17 +0530

    first commit test

---
+++ July_31_2024/sum_of_array_elements.java
@@ -0,0 +1,14 @@
+package July_31_2024;
+
+public class sum_of_array_elements {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int[] num = {1,2,3,4,5,6,7,8,9,10};
+        int sum=0;
+        for(int a: num){
+            sum+=a;
+        }
+        System.out.println(sum);
+
+    }
+}
