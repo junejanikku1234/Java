36d8cb66f7571f12dd83f01374ed0fb41c28f2d8
junejanikku1234 <23csu215@ncuinida.edu>
2024-08-01 21:40:44 +0530

    sequential search added

---
+++ random_string_arraylist.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.*;
+
+public class random_string_arraylist {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        ArrayList<String> str_lst = new ArrayList<String>();
+        Scanner con = new Scanner(System.in);
+
+    }
+
+    public static String getString(){
+        File file = new File("");
+    }
+}
