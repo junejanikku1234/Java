ac636b424f5c194e12c16b51548ce2860bfc6e4b
junejanikku1234 <23csu215@ncuinida.edu>
2024-08-23 22:54:03 +0530

    made changes to circular linked list

--- August_21_08_2024/circular_linked_list.java
+++ August_21_08_2024/circular_linked_list.java
@@ -2,7 +2,7 @@ package August_21_08_2024;
 
 public class circular_linked_list {
 
-    public class Node{
+    public class Node {
         int data;
         Node next;
         public Node(int data) {
@@ -10,9 +10,11 @@ public class circular_linked_list {
             this.next = null;
         }
     }
+
     private Node head = null;
     private Node tail = null;
 
+    // Add node to the list
     public void AddNode(int data) {
         Node newNode = new Node(data);
         if (head == null) {
@@ -26,97 +28,100 @@ public class circular_linked_list {
         }
     }
 
-    public void printList(){
-        if(head == null){
+    // Print list elements
+    public void printList() {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
         Node temp = head;
-
-        do{
-            System.out.print(temp.data+" ");
+        do {
+            System.out.print(temp.data + " ");
             temp = temp.next;
-        } while(temp != head);
+        } while (temp != head);
     }
 
-    public void deleteByValue(int value){
-        Node temp = head;
-        Node prev = null;
-
-        if(head == null){
+    // Delete node by value
+    public void deleteByValue(int value) {
+        if (head == null) {
             System.out.println("List is empty, nothing to delete");
             return;
         }
-        else if(head.data == value){
-            tail.next = head.next;
+
+        // Special case: only one node in the list
+        if (head == tail && head.data == value) {
+            head = null;
+            tail = null;
+            return;
+        }
+
+        Node current = head;
+        Node previous = tail;
+
+        // Check if head needs to be deleted
+        if (head.data == value) {
+            previous.next = head.next;
             head = head.next;
             return;
         }
-        do{
-            prev = temp;
-            temp = temp.next;
-        } while(temp != head && temp.data != value);
-        if(temp == head){
+
+        // Traverse the list to find the node to delete
+        do {
+            previous = current;
+            current = current.next;
+        } while (current != head && current.data != value);
+
+        if (current == head) {
             System.out.println("Element not found in the list");
-        }
-        prev.next = temp.next;
-        temp.next = null;
-        if(temp == tail){
-            tail = prev;
+            return;
         }
 
+        // Update pointers to remove the node
+        previous.next = current.next;
+
+        // Update tail if necessary
+        if (current == tail) {
+            tail = previous;
+        }
     }
 
+    // Delete node by index
     public void deleteByIndex(int index) {
-        // Check if the list is empty
         if (head == null) {
             System.out.println("List is empty.");
             return;
         }
 
-        // If the index is 0, delete the head node
+        // Special case: delete head node
         if (index == 0) {
-            if (head == tail) {
-                // If there's only one node
-                head = null;
-                tail = null;
-            } else {
-                // Delete the head and reassign it
-                tail.next = head.next; // Maintain circularity
-                head = head.next;       // Update head
-            }
+            deleteByValue(head.data);
             return;
         }
 
-        // Initialize pointers
         Node current = head;
         Node previous = null;
+        int count = 0;
 
         // Traverse the list to find the node at the specified index
-        int count = 0;
         do {
             previous = current;
             current = current.next;
             count++;
         } while (count != index && current != head);
 
-        // If the index is out of bounds (i.e., we reached back to the head)
         if (current == head) {
             System.out.println("Index out of bounds.");
             return;
         }
 
-        // Update the links to remove the node at the given index
+        // Update pointers to remove the node
         previous.next = current.next;
-        current.next = null;
 
-        // If we deleted the tail, update the tail reference
+        // Update tail if necessary
         if (current == tail) {
             tail = previous;
         }
     }
-
-
 
     public static void main(String[] args) {
         circular_linked_list list = new circular_linked_list();
@@ -124,9 +129,11 @@ public class circular_linked_list {
         list.AddNode(2);
         list.AddNode(3);
         list.printList();
+
         list.deleteByValue(1);
         System.out.println();
         list.printList();
+
         list.deleteByIndex(1);
         System.out.println();
         list.printList();
